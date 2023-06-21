6f5c95dec5e77ccb1f78f89ab83c6020e55c90f5
maxpsurcov <max.psurcov1@gmail.com>
2023-06-21 14:39:10 +0300

    Добавьте файлы проекта.

---
+++ OOPkursova7489/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace OOPkursova7489
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChangeButton = new System.Windows.Forms.Button();
+            this.ValueTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ChangeButton
+            // 
+            this.ChangeButton.Location = new System.Drawing.Point(77, 178);
+            this.ChangeButton.Name = "ChangeButton";
+            this.ChangeButton.Size = new System.Drawing.Size(153, 64);
+            this.ChangeButton.TabIndex = 0;
+            this.ChangeButton.Text = "ChangeButton";
+            this.ChangeButton.UseVisualStyleBackColor = true;
+            this.ChangeButton.Click += new System.EventHandler(this.ChangeButton_Click);
+            // 
+            // ValueTextBox
+            // 
+            this.ValueTextBox.Location = new System.Drawing.Point(77, 66);
+            this.ValueTextBox.Name = "ValueTextBox";
+            this.ValueTextBox.Size = new System.Drawing.Size(349, 29);
+            this.ValueTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ValueTextBox);
+            this.Controls.Add(this.ChangeButton);
+            this.Name = "Form1";
+            this.Text = "CustomDataType";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ChangeButton;
+        private System.Windows.Forms.TextBox ValueTextBox;
+    }
+}
+
---
+++ OOPkursova7489/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPkursova7489
+{
+    public partial class Form1 : Form
+    {
+        private CustomNumber customNum;
+        private ErrorProvider errorProvider;
+        public Form1()
+        {
+            InitializeComponent();
+
+            customNum = new CustomNumber("");
+            errorProvider = new ErrorProvider();
+        }
+
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+            string newValue = ValueTextBox.Text;
+
+            if (IsValidInput(newValue))
+            {
+                if (IsValidNumber(newValue))
+                {
+                    customNum.SetValue(newValue);
+                    customNum.PrintValue();
+                    errorProvider.Clear();
+                }
+                else
+                {
+                    errorProvider.SetError(ValueTextBox, "Некоректне число. Введіть ціле число в діапазоні від 1 до 100000000.");
+                }
+            }
+            else
+            {
+                errorProvider.SetError(ValueTextBox, "Некоректний ввід. Введіть тільки цифри зі знаком '+' або '-'.");
+            }
+        }
+        private bool IsValidInput(string value)
+        {
+            foreach (char c in value)
+            {
+                if (!char.IsDigit(c) && c != '+' && c != '-')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsValidNumber(string value)
+        {
+            bool isValid = false;
+
+            if (int.TryParse(value, out int number))
+            {
+                if (Math.Abs(number) >= 1 && Math.Abs(number) <= 100000000)
+                {
+                    isValid = true;
+                }
+            }
+
+            return isValid;
+        }
+    }
+    public class CustomNumber
+    {
+        private List<char> value;
+
+        public CustomNumber(string number)
+        {
+            value = new List<char>(number);
+        }
+
+        public CustomNumber(CustomNumber other)
+        {
+            value = new List<char>(other.value);
+        }
+
+        public void SetValue(string number)
+        {
+            value = new List<char>(number);
+        }
+
+        public string GetValue()
+        {
+            return new string(value.ToArray());
+        }
+
+        public void PrintValue()
+        {
+            MessageBox.Show(new string(value.ToArray()), "Custom Number");
+        }
+
+        public void ChangePart(string newValue)
+        {
+            if (IsValidInput(newValue))
+            {
+                value = new List<char>(newValue);
+            }
+            else
+            {
+                MessageBox.Show("Некоректний ввід. Введіть тільки цифри зі знаком '+' або '-'.", "Помилка");
+            }
+        }
+
+        public void DisplayContent()
+        {
+            MessageBox.Show(new string(value.ToArray()), "Custom Number");
+        }
+
+        public void Increment()
+        {
+            int number;
+            if (int.TryParse(new string(value.ToArray()), out number))
+            {
+                number++;
+                value = new List<char>(number.ToString());
+            }
+        }
+
+        public void Decrement()
+        {
+            int number;
+            if (int.TryParse(new string(value.ToArray()), out number))
+            {
+                number--;
+                value = new List<char>(number.ToString());
+            }
+        }
+
+        public void Negate()
+        {
+            int number;
+            if (int.TryParse(new string(value.ToArray()), out number))
+            {
+                number = -number;
+                value = new List<char>(number.ToString());
+            }
+        }
+
+        private bool IsValidInput(string value)
+        {
+            foreach (char c in value)
+            {
+                if (!char.IsDigit(c) && c != '+' && c != '-')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
